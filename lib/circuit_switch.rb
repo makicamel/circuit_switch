ab8231605abdff671d9698fc734c1a57c203c76a
makicamel <unright@gmail.com>
2021-09-19 18:52:02 +0900

    bundle gem circuit_switch

---
+++ lib/circuit_switch.rb
@@ -0,0 +1,6 @@
+require "circuit_switch/version"
+
+module CircuitSwitch
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/circuit_switch/version.rb
@@ -0,0 +1,3 @@
+module CircuitSwitch
+  VERSION = "0.1.0"
+end
