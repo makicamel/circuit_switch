3b14eb2aaaf48eb09e2c4810cfb8c1545fe835c7
makicamel <unright@gmail.com>
2021-09-23 08:33:44 +0900

    Be able to choice report contains backtrace or not

--- lib/circuit_switch/configuration.rb
+++ lib/circuit_switch/configuration.rb
@@ -1,10 +1,14 @@
 module CircuitSwitch
   class Configuration
     attr_accessor :reporter
-    attr_writer :report_paths
+    attr_writer :report_paths, :with_backtrace
 
     def report_paths
       @report_paths ||= [Rails.root]
+    end
+
+    def with_backtrace
+      @with_backtrace ||= false
     end
   end
 end
--- lib/generators/circuit_switch/templates/initializer.rb
+++ lib/generators/circuit_switch/templates/initializer.rb
@@ -8,4 +8,9 @@ CircuitSwitch.configure do |config|
   # Allowed paths to report
   # CircuitSwitch recognizes logic as unique that first match with these paths
   # config.report_paths = [Rails.root]
+
+  # Option to contain error backtrace for report
+  # You don't need backtrace when you report to some bug report tool.
+  # You may be want backtrace when report to plain feed; e.g. Slack or email.
+  # config.with_backtrace = false
 end
