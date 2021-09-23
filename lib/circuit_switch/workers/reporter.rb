857acd70e1f88fdb4843ab3fc7d1f80e6f98dbb8
makicamel <unright@gmail.com>
2021-09-23 19:29:17 +0900

    Strip called_path

--- lib/circuit_switch/workers/reporter.rb
+++ lib/circuit_switch/workers/reporter.rb
@@ -5,13 +5,21 @@ module CircuitSwitch
     delegate :config, to: ::CircuitSwitch
 
     def perform
-      called_path = caller.detect { |path| path.match?(/(#{config.report_paths.join('|')})/) } || "/somewhere/in/library:in #{Date.today}"
       circuit_switch = CircuitSwitch.find_or_initialize_by(caller: called_path)
       if circuit_switch.watching?
         circuit_switch.increment
         notification = raise CalledNotification.new(circuit_switch.message) rescue $!
         config.reporter.call(notification.to_message(called_path: called_path))
       end
+    end
+
+    private
+
+    def called_path
+      @called_path ||= caller
+        .detect { |path| path.match?(/(#{config.report_paths.join('|')})/) }
+        &.sub(/(#{config.strip_paths.join('|')})/, '') ||
+        "/somewhere/in/library:in #{Date.today}"
     end
   end
 end
